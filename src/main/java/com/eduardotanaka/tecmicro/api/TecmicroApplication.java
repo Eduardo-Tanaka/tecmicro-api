1a947f397c2569d03e3148b446ef0f834f6a78d4
Eduardo <eduardo.s.tanaka@gmail.com>
2018-07-14 16:56:16 -0300

    initial commit

---
+++ src/main/java/com/eduardotanaka/tecmicro/api/TecmicroApplication.java
@@ -0,0 +1,12 @@
+package com.eduardotanaka.tecmicro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecmicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecmicroApplication.class, args);
+	}
+}
